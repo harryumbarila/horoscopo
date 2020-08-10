261163b623d810674ff52313e199a5ae38a27f2f
Harrison Umbarila <harry96sn@gmail.com>
2020-08-09 23:55:54 -0500

    primer commite horoscopo

---
+++ horoscopo.js
@@ -0,0 +1,43 @@
+var signo = prompt("Cual es tu signo?")
+
+switch(signo){
+    case "aries":
+    document.write("Entienda que si se deja guiar por la voz de la intuición y la sabiduría interior, pronto podrá abandonar la inseguridad y los temores que lo persiguen.")
+    break
+    case "tauro":
+    document.write("Sepa que iniciará el día decidido a concretar un par de objetivos que tenía entre manos. Aproveche las buenas energías que le brindará la Luna.")
+    break
+    case "geiminis":
+    document.write("Entienda que debe mantenerse precavido, ya que podrían aparecer personas envidiosas que intenten retardar su éxito. Procure alejarse de las mismas.")
+    break
+    case "cancer":
+    document.write("No permita que la pena invada su corazón. Procure no cometer un error por su impaciencia, intente relajarse y conseguirá lo que se propuso en la vida.")
+    break
+    case "leo":
+    document.write("Fase óptima para reformar su vida y su círculo social. Trate de controlar los repentinos cambios de humor que últimamente lo afectan en las relaciones.")
+    break
+    case     "virgo":
+    document.write("Intente confiar más en su capacidad intelectual. Muchos de los esfuerzos y metas se verán limitados por la inseguridad que lo invade naturalmente.")
+    break
+    case "libra":
+    document.write("Aprenda a mantener siempre la serenidad y la confianza en su espíritu. Sepa que debe comprender que ambas son la llave de la sabiduría en la vida.")
+    break
+    case "escorpio":
+    document.write("Procure no desesperarse e intente sostener la calma. Durante esta jornada, una situación un tanto confusa podría llegar a desordenarlo anímicamente.")
+    break
+    case "sagitario":
+    document.write("Alimente su jovialidad, así podrá eliminar los sentimientos negativos. En poco tiempo, resurgirá la fuerza de su personalidad y se sentirá más vital.")
+    break
+    case "capricornio":
+    document.write("Haga lo posible para cumplir con todos los deseos de un modo constructivo. Intente abandonar los problemas que no le afectan directamente a su vida.")
+    break
+    case "acuario":
+    document.write("Sepa que podrá acrecentar los vínculos con esos familiares olvidados. Entienda que una llamada a sus parientes para saber cómo andan será la mejor opción.")
+    break
+    case "piscins":
+    document.write("Comprenda que fortaleciendo el entusiasmo y la confianza pronto se acentuará su capacidad imaginativa y fantasiosa en todos los objetivos que emprenda.")
+    break
+    default:
+    document.write("No es un signo zodiacal valido, intente nuevamente por favor.")
+    break
+}
